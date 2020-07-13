0fc00d238d3ad782a3d4c86baf109e588288261e
mobile08092018 <kakvospitat@gmail.com>
2020-07-13 09:53:57 +0300

    '13.07.2020-export-import'

---
+++ src/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export function Header() {
+    return (
+        <div>
+            <a>Home</a>
+            <a>News Feed</a>
+            <a>Messages</a>
+        </div>
+    );
+}
---
+++ src/Technologies.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>Html</li>
+                <li>CSS</li>
+                <li>JS</li>
+            </ul>
+        </div>
+    );
+}
