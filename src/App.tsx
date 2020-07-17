a0ab4fad5832485c64c1fe6c9af309f25222f599
mobile08092018 <kakvospitat@gmail.com>
2020-07-17 14:16:38 +0300

    17.07.2020-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
-import {Header} from './Header'
-import {Technologies} from './Technologies'
 import './App.css';
+import {Header} from "./components/Header";
+import {Navbar} from "./components/Navbar";
+import {Profile} from "./components/Profile";
 
 function App() {
     return (
-        <div>
+        <div className={'Wrapper'}>
             <Header/>
-            <Technologies/>
+            <Navbar/>
+            <Profile/>
+            <div className={'Footer'}>Footer</div>
+
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export let Header=()=>{
+    return(
+        <header className={'Header'}>
+             <img src={'https://pngimg.com/uploads/sony_logo/sony_logo_PNG7.png'}/>
+        </header>
+    )
+}
+
