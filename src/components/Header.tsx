28d4e866fc9e8032d2b931d631f47af6055c7a38
mobile08092018 <kakvospitat@gmail.com>
2020-07-20 20:58:20 +0300

    20.07.2020-css modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from './Header.module.css';
 
 export let Header=()=>{
     return(
-        <header className={'Header'}>
+        <header className={classes.Header}>
              <img src={'https://pngimg.com/uploads/sony_logo/sony_logo_PNG7.png'}/>
         </header>
     )
