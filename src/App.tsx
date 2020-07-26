a09fdf345d719cf5f30ef51fb3ca8274abc029f1
mobile08092018 <kakvospitat@gmail.com>
2020-07-26 09:02:06 +0300

    26.07.2020-dialogs

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
+import classes from "*.module.css";
 
 function App() {
     return (
         <div className={'Wrapper'}>
             <Header/>
             <Navbar/>
-            <Profile/>
+            {/*<Profile/>*/}
+
+            <div className={'app-wrapper-content'}><Dialogs/></div>
             <div className={'Footer'}>Footer</div>
 
         </div>
