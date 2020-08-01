9185e3f0aed4af8e68faf0eea9459170c5ac5534
mobile08092018 <kakvospitat@gmail.com>
2020-08-01 19:46:11 +0300

    26.07.2020-Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import {Route, BrowserRouter} from 'react-router-dom'
 import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navbar} from "./components/Navbar/Navbar";
 import {Profile} from "./components/Profile/Profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
-import classes from "*.module.css";
+import {Body} from "./components/Body/Body";
+
 
 function App() {
     return (
-        <div className={'Wrapper'}>
-            <Header/>
-            <Navbar/>
-            {/*<Profile/>*/}
+        <BrowserRouter>
+            <div className={'Wrapper'}>
+                <Header/>
+                <Navbar/>
+                <div className={'app-wrapper-content'}>
+                    <Route exact path='/' component={Body}/>
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                </div>
+                <div className={'Footer'}>Footer</div>
+            </div>
+        </BrowserRouter>
 
-            <div className={'app-wrapper-content'}><Dialogs/></div>
-            <div className={'Footer'}>Footer</div>
-
-        </div>
     );
 }
-
 
 export default App;
