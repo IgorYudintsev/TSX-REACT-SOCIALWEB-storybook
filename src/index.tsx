85d2b6b04bfb3acc77286903bf9136a3e4ee3934
mobile08092018 <kakvospitat@gmail.com>
2020-09-05 18:00:05 +0300

    5.09.2020. 37. Уроки, Курс React JS - store, state, ООП, рефакторинг

--- src/index.tsx
+++ src/index.tsx
@@ -2,33 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import state, {addPost, stateType, subscribe, updateNewPostText} from './redux/state'
+import store, {stateType} from './redux/state'
 import {BrowserRouter} from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
-
 
 let rerenderEntireTree=(state:stateType)=>{
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App state={state}
-                     addPost={addPost}
-                     updateNewPostText={updateNewPostText}
+                <App state={store.getState()}
+                     addPost={store.addPost.bind(store)}//bind-следит за this
+                     updateNewPostText={store.updateNewPostText.bind(store)}
                 />
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
-
-
-
-
-
-
-
+rerenderEntireTree(store.getState());//запускаем перерисовку->т.е. стр.9
+store.subscribe(rerenderEntireTree);//отправляет rerenderEntireTree в state.tsx
+//чтобы там можно было перезагрузить rerenderEntireTree из state.tsx
 
 
 serviceWorker.unregister();
--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,25 +1,24 @@
-
 import React from 'react';
-
-let rerenderEntireTree=(state: stateType)=>{
-    console.log('state was changed')
-}
-
+export type storeType = {
+    _state: stateType
+    getState: () => stateType
+    _callSubsriber: (state: stateType) => void
+    addPost: () => void
+    updateNewPostText: (newText: string) => void
+    subscribe: (observer: (state: stateType) => void) => void
+}//типизация storage
 export type stateType = {
     profilePage: profilePageType
     dialogsPage: messagesPageType
 }
-
 export type messagesPageType = {
     messages: Array<MessagesType>
     dialogs: Array<DialogsType>
 }
-
 export type profilePageType = {
     posts: Array<postsType>
     newPostText: string
 }
-
 export type postsType = {
     id: number
     message: string
@@ -34,47 +33,58 @@ export type MessagesType = {
     message: string
 }
 
-let state: stateType = {
-    profilePage: {
-        posts: [
-            {id: 1, message: 'Hi', likesCount: 10},
-            {id: 2, message: 'How are you?', likesCount: 100}
-        ],
-        newPostText: ''
+let store: storeType = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: 'Hi', likesCount: 10},
+                {id: 2, message: 'How are you?', likesCount: 100}
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            messages: [
+                {id: 1, message: 'Hi'},
+                {id: 2, message: 'How are you?'},
+                {id: 3, message: 'Yoo'},
+                {id: 4, message: 'How are you?'},
+                {id: 5, message: 'Yoo'},
+            ],
+            dialogs: [
+                {id: 1, name: 'Dimach'},
+                {id: 2, name: 'Sasha'},
+                {id: 3, name: 'Igor'},
+                {id: 4, name: 'Olga'},
+                {id: 5, name: 'Nadzeika'},
+            ]
+        }
+    },//стейт
+    getState: function () {
+        return this._state;
+    },//выдача стейта
+    _callSubsriber: function (state: stateType) {
+        console.log('state was changed')
     },
-    dialogsPage: {
-        messages: [
-            {id: 1, message: 'Hi'},
-            {id: 2, message: 'How are you?'},
-            {id: 3, message: 'Yoo'},
-            {id: 4, message: 'How are you?'},
-            {id: 5, message: 'Yoo'},
-        ],
-        dialogs: [
-            {id: 1, name: 'Dimach'},
-            {id: 2, name: 'Sasha'},
-            {id: 3, name: 'Igor'},
-            {id: 4, name: 'Olga'},
-            {id: 5, name: 'Nadzeika'},
-        ]
-    }
+    addPost: function () {//добавление поста
+        let newPosts = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+        this._state.profilePage.posts.push(newPosts);
+        this._state.profilePage.newPostText = '';
+        this._callSubsriber(this._state);
+    },
+    updateNewPostText: function (newText: string) {//onChange
+        this._state.profilePage.newPostText = newText
+        this._callSubsriber(this._state);
+    },
+    subscribe: function (observer: (state: stateType) => void) {
+        this._callSubsriber = observer;
+    }//в index.tsx мы запустили эту функцию
+    //мы передали сюда rerenderEntireTree(со стейтом внутри)
+    //и сейчас _callSubsriber в котором левый мусор станет
+    // rerenderEntireTree(со стейтом внутри)
 }
 
-export let addPost = () => {
-    let newPosts = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0};
-    state.profilePage.posts.push(newPosts);
-    state.profilePage.newPostText='';
-    rerenderEntireTree(state);
-}
-export let updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText=newText
-    rerenderEntireTree(state);
-}
-export const subscribe=(observer:(state: stateType)=>void)=>{
-    rerenderEntireTree=observer;
-}
-
-export default state;
+export default store;
